2b0e27ceaed84307ec4709bcb62e35951af81948
ITE03050114 <03050114@me.mcu.edu.tw>
2016-10-17 19:39:56 +0800

    client

--- client.c
+++ client.c
@@ -4,18 +4,23 @@
 #include<arpa/inet.h> //inet_addr
 int main(int argc , char *argv[])
 {
-int socket_desc;
+int socket_desc,port;
 struct sockaddr_in server;
-char *message , server_reply[2000];
+char message[50] , server_reply[2000];
+char IP[20];
 //Create socket
 socket_desc = socket(AF_INET , SOCK_STREAM , 0);
 if (socket_desc == -1)
 {
 printf("Could not create socket");
 }
-server.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+scanf("%s",IP);
+scanf("%d",&port);
+
+server.sin_addr.s_addr = inet_addr(IP);
 server.sin_family = AF_INET;
-server.sin_port = htons( 8888 );
+server.sin_port = htons( port );
 //Connect to remote server
 if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
 {
@@ -23,20 +28,25 @@ puts("connect error");
 return 1;
 }
 puts("Connected\n");
-//Send some data
-message = "GET / HTTP/1.1\r\n\r\n";
-if( send(socket_desc , message , strlen(message) , 0) < 0)
+
+while(1)
 {
-puts("Send failed");
-return 1;
+	//Send some data
+	//message = "GET / HTTP/1.1\r\n\r\n";
+	scanf("%s",message);
+	if( send(socket_desc , message , strlen(message) , 0) < 0)
+	{
+	puts("Send failed");
+	return 1;
+	}
+	puts("Data Send\n");
+	//Receive a reply from the server
+	if( recv(socket_desc, server_reply , 2000 , 0) < 0)
+	{
+	puts("recv failed");
+	}
+	puts("Reply received\n");
+	puts(server_reply);
 }
-puts("Data Send\n");
-//Receive a reply from the server
-if( recv(socket_desc, server_reply , 2000 , 0) < 0)
-{
-puts("recv failed");
-}
-puts("Reply received\n");
-puts(server_reply);
 return 0;
 }
