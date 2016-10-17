a99b360ce188de541e6f21425f329c3141868334
ITE03050114 <03050114@me.mcu.edu.tw>
2016-10-17 19:39:21 +0800

    server

--- server.c
+++ server.c
@@ -12,7 +12,7 @@ mytime=time(NULL);
 
 int socket_desc , new_socket , c;
 struct sockaddr_in server , client;
-char *message;
+char message[100];
 //Create socket
 socket_desc = socket(AF_INET , SOCK_STREAM , 0);
 if (socket_desc == -1)
@@ -22,7 +22,7 @@ printf("Could not create socket");
 //Prepare the sockaddr_in structure
 server.sin_family = AF_INET;
 server.sin_addr.s_addr = INADDR_ANY;
-server.sin_port = htons( 8888 );
+server.sin_port = htons( 8787 );
 //Bind
 if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
 {
@@ -39,8 +39,15 @@ while( (new_socket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*
 {
 puts("Connection accepted");
 //Reply to the client
-message = ctime(&mytime);
-write(new_socket , message , strlen(message));
+while(1)
+{
+	bzero(message,100);
+	read(new_socket,message,100);
+	printf("Echoing back - %s",message);
+	write(new_socket , message, strlen(message)+1);
+}
+//message = ctime(&mytime);
+//write(new_socket , message , strlen(message));
 }
 if (new_socket<0)
 {
